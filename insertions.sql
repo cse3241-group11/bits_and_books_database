800574268e36e58b0986b98764933820384286dc
Hanson <hansonma2021@gmail.com>
2022-10-28 21:25:51 -0400

    changes

--- insertions.sql
+++ insertions.sql
@@ -1,1 +1,31 @@
-INSERT INTO
+--WAREHOUSE
+INSERT INTO WAREHOUSE VALUES(0,'A', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'B', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'C', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'D', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'E', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'F', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'G', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'H', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'I', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'J', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'K', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'L', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'O', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'P', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'Q', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'R', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'S', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'T', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'U', '', '', '', '', '');
+INSERT INTO WAREHOUSE VALUES(0,'V', '', '', '', '', '');
+
+--EMPLOYEE
+--INSERT INTO EMPLOYEE VALUES('111111111', 'Hanson', null, 'Ma', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111112', 'Neha', null, 'Checka', null, 20000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111113', 'Ted', null, 'Li', null, 30000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111114', 'Alexandra', null, 'Kashani', null, 40000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111115', 'Odin', null, 'Acevedo', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111116', 'Jacqueline', null, 'Piper', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111117', 'Keanu', null, 'Crosby', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111118', 'asdf', null, 'asdf', null, 10000, null, null, null, null, null, null);
