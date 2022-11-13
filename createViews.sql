899992fa7d10905da6e7c82db1f784560bb82d32
Hanson <hansonma2021@gmail.com>
2022-11-13 16:58:56 -0500

    added descriptions to views

--- createViews.sql
+++ createViews.sql
@@ -1,4 +1,7 @@
 --first view
+--gets the name, email, and total spent with us of each customer and orders
+-- them in descending order base on the total spent to determine the most
+-- loyal customers/customers that spent the most
 CREATE VIEW IF NOT EXISTS most_loyal_customers
 AS
     SELECT First_Name, M_init, Last_Name, email, SUM(Total_cost) AS Total_c
@@ -8,7 +11,9 @@ AS
     ORDER BY Total_c DESC;
 
 --second view
-CREATE VIEW IF NOT EXISTS view_name
+--gets the ISBN, title, and publisher price of the most popular books (based
+-- on number of transactions that ISBN was part of in descending order)
+CREATE VIEW IF NOT EXISTS most_popular_books
 AS
     SELECT BOOK.ISBN, Title, publisher_price, COUNT(*) AS Total_p
     FROM BOOK, PURCHASES
