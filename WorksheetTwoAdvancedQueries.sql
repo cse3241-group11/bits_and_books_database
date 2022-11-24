459b802c740d67a6a1e721e0fef8a584d81c72e0
nchecka056 <checkaneha@gmail.com>
2022-11-23 20:20:42 -0500

    simple query changes for worksheet 2

--- WorksheetTwoAdvancedQueries.sql
+++ WorksheetTwoAdvancedQueries.sql
@@ -1,3 +1,30 @@
+--4A
+
+SELECT CUSTOMER.Customer_ID, CUSTOMER.First_Name, CUSTOMER.Last_Name, Book.Title
+FROM Published_By, Book, Customer, Publisher, Purchases
+WHERE Publisher_Name = 'HarperCollins'
+AND Publisher.Publisher_ID = Published_By.Publisher_ID
+AND PUBLISHED_BY.ISBN = Book.ISBN
+AND Purchases.ISBN = Book.ISBN
+AND Purchases.Customer_ID = Customer.Customer_ID;
+
+--4b.
+
+SELECT SUM (Quantity) AS Total_Q
+FROM Customer, Publisher, Purchases, PUBLISHED_BY, BOOK
+WHERE CUSTOMER.First_Name = 'Samantha'
+AND Customer.Customer_ID = Purchases.Customer_ID
+AND Publisher.Publisher_Name = 'HarperCollins'
+AND Publisher.Publisher_ID = Published_By.Publisher_ID
+AND Purchases.ISBN = Book.ISBN
+AND PUBLISHED_BY.ISBN = Book.ISBN;
+
+--4c
+SELECT AVG(Salary) AS Avg_Sal
+FROM Employee
+NATURAL JOIN Administrator;
+
+
 --5a
 SELECT First_name, Last_name, SUM (Total_Cost) As CustTotal
 FROM Customer, Purchases
