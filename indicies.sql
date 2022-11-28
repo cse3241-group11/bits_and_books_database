cac65ee507109cb54838f85b66bccd0082d1c774
Ted Li <li.6998@osu.edu>
2022-11-27 19:58:08 -0500

    indicies.sql some new indicies again

--- indicies.sql
+++ indicies.sql
@@ -4,8 +4,12 @@
 CREATE INDEX author_names ON Author(First_Name, Middle_Name, Last_Name);
 CREATE INDEX customer_names ON Customer(First_Name, M_init, Last_Name);
 CREATE INDEX employee_names ON Employee(First_Name, M_init, Last_Name);
--- ETC...
+CREATE INDEX publisher_names ON PUBLISHER(Publisher_Name);
+CREATE INDEX warehouse_names ON WAREHOUSE(Name);
 
--- Since looking up transactions by dates is another common task, indices on such columns would be beneficial as well
+-- Since looking up transactions and tuples by dates is another common task, indices on such columns would be
+-- beneficial as well
+CREATE INDEX book_published_dates ON Book(published_date);
+CREATE INDEX employee_bdates ON Employee(Bdate);
 CREATE INDEX order_dates ON Orders(Order_date);
 CREATE INDEX purchase_dates ON Purchases(Purchase_date);
