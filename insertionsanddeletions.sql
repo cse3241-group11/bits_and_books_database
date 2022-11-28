303e4c28a9e8983118ed34f579c814169d74d3c3
Ted Li <li.6998@osu.edu>
2022-11-28 03:57:58 -0500

    Added comments for insertions in insertionsanddeletions.sql

--- insertionsanddeletions.sql
+++ insertionsanddeletions.sql
@@ -51,15 +51,19 @@ INSERT INTO ADMINISTRATOR VALUES('111111114');
 INSERT INTO ADMINISTRATOR VALUES('111111115');
 
 --BOOK
+-- Insert into book table information about the book titled "The Book Thief"
 INSERT INTO BOOK VALUES ('0235670861','The Book Thief' ,'0.33','0.58','50','2002-01-01');
+-- Insert into book table information about the book titled "It Ends with Us: A Novel"
 INSERT INTO BOOK VALUES ('1501110365', 'It Ends with Us: A Novel', '0.37', '0.43', '95', '2016-08-02');
 --INSERT INTO BOOK VALUES ('ISBN', 'Title', 'discount price', 'margin percent', 'publisher_price', 'published_date');
 
 --PUBLISHER
+-- Insert into publisher table information about the publisher named "Addison-Nicholas Pub Co"
 INSERT INTO PUBLISHER (Publisher_street, Publisher_city, Publisher_state, Publisher_zip,
                                                  Publisher_Name, publisher_ordering_website) VALUES
                       ('70532 Preston Underpass','New Nicholestad','DE','19979','Addison-Nicholas Pub Co',
                                                                                 'db-913.diaz.larsen.net');
+-- Insert into publisher table information about the publisher named "Jossey-Guitar"
 INSERT INTO PUBLISHER (Publisher_street, Publisher_city, Publisher_state, Publisher_zip,
                                                 Publisher_Name, publisher_ordering_website) VALUES
                       ('6299 Robinson Mountains Apt. 387', 'Riggsborough', 'MD', '21883', 'Jossey-Guitar',
@@ -73,10 +77,12 @@ INSERT INTO PUBLISHER (Publisher_street, Publisher_city, Publisher_state, Publis
  */
 
 --AUTHOR
+-- Insert into author table information the author named "Nicole-László Berebási"
 INSERT INTO AUTHOR (First_Name, Middle_Name, Last_Name) VALUES ('Nicole-László', null, 'Berebási');
 --INSERT INTO AUTHOR (First_Name, Middle_Name, Last_Name)  VALUES ('First_Name', 'Middle_Name', 'Last_Name');
 
 --CUSTOMERS
+-- Insert into customer table information the customer named "Daniel E Glover"
   INSERT INTO CUSTOMER (First_Name, M_init, Last_Name, Phone_Number, Billing_street, Billing_city,
                                   Billing_state, Billing_zip, Shipping_street, Shipping_city, Shipping_state,
                                   Shipping_zip,email,password, card_number, card_sec_code, card_expiration_date,
