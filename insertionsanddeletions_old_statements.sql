d005d92513c4999a89772b1b0dade50c230c422f
Ted Li <li.6998@osu.edu>
2022-11-28 23:11:15 -0500

    Reorganized insertionsanddeletions.sql into two different files

---
+++ insertionsanddeletions_old_statements.sql
@@ -0,0 +1,51 @@
+--WAREHOUSE
+INSERT INTO WAREHOUSE VALUES(0,'A', '', '', '', '', '111111111');
+INSERT INTO WAREHOUSE VALUES(1,'B', '', '', '', '', '111111112');
+INSERT INTO WAREHOUSE VALUES(2,'C', '', '', '', '', '111111113');
+INSERT INTO WAREHOUSE VALUES(3,'D', '', '', '', '', '111111114');
+INSERT INTO WAREHOUSE VALUES(4,'E', '', '', '', '', '111111115');
+-- INSERT INTO WAREHOUSE VALUES(0,'F', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'G', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'H', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'I', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'J', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'K', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'L', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'O', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'P', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'Q', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'R', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'S', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'T', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'U', '', '', '', '', '');
+-- INSERT INTO WAREHOUSE VALUES(0,'V', '', '', '', '', '');
+
+--EMPLOYEE
+-- INSERT INTO WAREHOUSE VALUES('SSN','First_Name', 'M_init', 'Last_Name', 'Bdate', 'Salary', 'E_street', 'E_city', 'E_state', 'E_zip', 'Warehouse_number', 'Administrator_SSN');
+INSERT INTO EMPLOYEE VALUES('111111111', 'Hanson', null, 'Ma', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111112', 'Neha', null, 'Checka', null, 20000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111113', 'Ted', null, 'Li', null, 30000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111114', 'Alexandra', null, 'Kashani', null, 40000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111115', 'Odin', null, 'Acevedo', null, 10000, null, null, null, null, null, null);
+INSERT INTO EMPLOYEE VALUES('111111116', 'Jacqueline', null, 'Piper', null, 10000, null, null, null, null, 0, '111111111');
+INSERT INTO EMPLOYEE VALUES('111111117', 'Keanu', null, 'Crosby', null, 10000, null, null, null, null, 0, '111111111');
+INSERT INTO EMPLOYEE VALUES('111111118', 'asdf', null, 'asdf', null, 10000, null, null, null, null, 0, '111111111');
+INSERT INTO EMPLOYEE VALUES('111111119', 'Kate', null, 'Hull', null, 10000, null, null, null, null, 1, '111111112');
+INSERT INTO EMPLOYEE VALUES('111111110', 'Amanda', null, 'Rodriguez', null, 20000, null, null, null, null, 1, '111111112');
+INSERT INTO EMPLOYEE VALUES('011111111', 'Ted', null, 'Ford', null, 30000, null, null, null, null, 1, '111111112');
+INSERT INTO EMPLOYEE VALUES('001111112', 'Alex', null, 'Smith', null, 40000, null, null, null, null, 2, '111111113');
+INSERT INTO EMPLOYEE VALUES('001111113', 'Jack', null, 'Black', null, 10000, null, null, null, null, 2, '111111113');
+INSERT INTO EMPLOYEE VALUES('011111114', 'Leo', null, 'Oliver', null, 40000, null, null, null, null, 2, '111111113');
+INSERT INTO EMPLOYEE VALUES('011111115', 'Nina', null, 'Patel', null, 20000, null, null, null, null, 3, '111111114');
+INSERT INTO EMPLOYEE VALUES('011111116', 'Ania', null, 'Brzozka', null, 30000, null, null, null, null, 3, '111111114');
+INSERT INTO EMPLOYEE VALUES('011111117', 'Patrick', null, 'Yang', null, 10000, null, null, null, null, 3, '111111114');
+INSERT INTO EMPLOYEE VALUES('011111118', 'Liyah', null, 'Bouza', null, 20000, null, null, null, null, 4, '111111115');
+INSERT INTO EMPLOYEE VALUES('011111119', 'Eddie', null, 'Allen', null, 30000, null, null, null, null, 4, '111111115');
+INSERT INTO EMPLOYEE VALUES('011111120', 'Nia', null, 'Smith', null, 40000, null, null, null, null, 4, '111111115');
+
+--ADMINISTRATOR
+INSERT INTO ADMINISTRATOR VALUES('111111111');
+INSERT INTO ADMINISTRATOR VALUES('111111112');
+INSERT INTO ADMINISTRATOR VALUES('111111113');
+INSERT INTO ADMINISTRATOR VALUES('111111114');
+INSERT INTO ADMINISTRATOR VALUES('111111115');
